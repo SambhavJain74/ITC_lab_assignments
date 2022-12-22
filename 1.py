e5562f7c0b5800dff0558657f7f25cfcd3f49367
SambhavJain74 <121171702+SambhavJain74@users.noreply.github.com>
2022-12-22 22:58:56 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,4 @@
+a=int(input("Enter the first integer"))
+b=int(input("Enter the second integer"))
+c=int(input("Enter the third integer"))
+print((a+b+c)/3)
---
+++ 3.py
@@ -0,0 +1,3 @@
+seconds=int(input("Enter the no of seconds"))
+print("no of minutes is ",(seconds/60)-(seconds%60)/60)
+print("no of seconds is ", seconds%60)
