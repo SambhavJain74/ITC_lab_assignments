7b4210b84393e25dc720b6fb1b0ac62bc7f9fd75
SambhavJain74 <121171702+SambhavJain74@users.noreply.github.com>
2023-01-04 17:18:10 +0530

    Add files via upload

--- Assignment-3/question_2.py
+++ Assignment-3/question_2.py
@@ -1,29 +1,47 @@
-a=int(input("Enter Date[1-31]:"))
-b=int(input("Enter Month[1-12]:"))
-c=int(input("Enter Year[1800-2025]:"))
-if a>31:
-    print("Error input correct date")
-    exit()
-#seeing if input year is a leap year or no
-if (c%4==0):
-    leapyear=True
+a=0
+def ask_date():
+    global a
+    a=int(input("Enter Date[1-31]:"))
+    if (a<1) or (a>31):
+        print("Enter a date from 1-31 only")
+        ask_date()
+
+b=0
+def ask_month():
+    global b
+    b=int(input("Enter Month[1-12]:"))
+    if (b<1) or (b>12): 
+        print("Enter a month from 1 to 12 only")
+        ask_month()
+
+c=0
+def ask_year(): 
+    global c
+    c=int(input("Enter Year[1800-2025]:"))
+    if (c<1800) or (c>2025):
+        print("Enter an year from 1800 to 2025 only")
+        ask_year()
+
+ask_date()
+ask_month()
+ask_year()
+
+if ((c%4==0)and(c%100!=0)) or c%400==0:
+    ly=True
 else:
-    leapyear=False
-#defining month length
+    ly=False
+
 if b in [1,3,5,7,8,10,12]:
     monthlength=31
 elif b in [4,6,9,11]:
     monthlength=30
+
 if b==2:
-    if leapyear:
+    if ly:
         monthlength=29
     else:
         monthlength=28
 
-if a>monthlength:
-    print("Error please input a correct date")
-    exit()
-#adding a day
 if a<monthlength:
     a=a+1
 else:
