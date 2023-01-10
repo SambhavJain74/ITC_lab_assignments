5b69e113d960b98c31d3dc10bc1efc37d0d2d1e9
SambhavJain74 <121171702+SambhavJain74@users.noreply.github.com>
2023-01-10 14:19:22 +0530

    Update q8.py

--- Loops Assignment/q8.py
+++ Loops Assignment/q8.py
@@ -4,20 +4,36 @@ for i in range(10):
     list1.append(t)
 print(list1)
 
+print("The positive numbers in the list are:")
 for t in list1:
-    print("The positive numbers in the list are:")
     if t>0:
         print(t)
 
+print("The negative numbers in the list are:")
 for t in list1:
     if t<0:
         print(t)
 
+print("The odd numbers in the list are:")
 for t in list1:
     if t%2!=0:
         print(t)
 
+print("The even numbers in the list are:")
 for t in list1:
     if t%2==0:
         print(t)
 
+'''no_of_occurences=dict()
+def histogram(s):
+    global no_of_occurences
+    for i in s:
+        no_of_occurences[i]=s.count(i)
+        s.remove(i)
+
+histogram(list1)
+print(no_of_occurences)'''
+
+number_set=[*set(list1)]
+for i in number_set:
+    print(i,"occurs",list1.count(i),"times")
