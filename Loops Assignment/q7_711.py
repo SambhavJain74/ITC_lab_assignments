82625c6060f2fc17f04c7eb1c19f6bfdf9192100
Sambhav Jain <121171702+SambhavJain74@users.noreply.github.com>
2023-01-19 19:42:38 +0530

    Update q7_711.py

--- Loops Assignment/q7_711.py
+++ Loops Assignment/q7_711.py
@@ -1,3 +1,4 @@
+#It isnt clear whether question is printing numbers divisible by both 7 and 11 or atleast one, if-conditions for both have been written
 for i in range(1,501):
     if i%7==0 or i%11==0:
         print(i)
